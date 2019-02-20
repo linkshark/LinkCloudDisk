cb53f32954359e87689fbd655a247af70c5f9a6b
sharkshen <35325899+sharkshen@users.noreply.github.com>
2019-02-20 16:18:36 +0800

    TestDemo添加

---
+++ service-base/src/test/java/com/linkjb/servicebase/ServiceBaseApplicationTests.java
@@ -0,0 +1,20 @@
+package com.linkjb.servicebase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiceBaseApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+
+}
