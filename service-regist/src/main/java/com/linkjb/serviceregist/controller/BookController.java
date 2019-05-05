1862ef0bcd87550175486a73ad3c0184bde5c3a4
linkshark <sharkshen@outlook.com>
2019-05-05 17:16:17 +0800

    service-regist 引入rabbitmq依赖

--- service-regist/src/main/java/com/linkjb/serviceregist/controller/BookController.java
+++ service-regist/src/main/java/com/linkjb/serviceregist/controller/BookController.java
@@ -11,7 +11,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
