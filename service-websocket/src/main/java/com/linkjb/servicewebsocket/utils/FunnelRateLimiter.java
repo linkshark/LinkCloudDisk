02d5c59f3b0e4ab079ecd5b9d68883be018ad591
linkshark <sharkshen@outlook.com>
2019-07-23 15:09:58 +0800

    redis 漏斗限流算法

---
+++ service-websocket/src/main/java/com/linkjb/servicewebsocket/utils/FunnelRateLimiter.java
@@ -0,0 +1,60 @@
+package com.linkjb.servicewebsocket.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author sharkshen
+ * @description
+ * @data 2019/7/23 14:50
+ */
+public class FunnelRateLimiter {
+    static class Funnel {
+        int capacity;
+        float leakingRate;
+        int leftQuota;
+        long leakingTs;
+        public Funnel(int capacity, float leakingRate) {
+            this.capacity = capacity;
+            this.leakingRate = leakingRate;
+            this.leftQuota = capacity;
+            this.leakingTs = System.currentTimeMillis();
+        }
+        void makeSpace() {
+            long nowTs = System.currentTimeMillis();
+            long deltaTs = nowTs - leakingTs;
+            int deltaQuota = (int) (deltaTs * leakingRate);
+            if (deltaQuota < 0) { // 间隔时间太长，整数数字过大溢出
+                this.leftQuota = capacity;
+                this.leakingTs = nowTs;
+                return;
+            }
+            if (deltaQuota < 1) { // 腾出空间太小，最小单位是 1
+                return;
+            }
+            this.leftQuota += deltaQuota;
+            this.leakingTs = nowTs;
+            if (this.leftQuota > this.capacity) {
+                this.leftQuota = this.capacity;
+            }
+        }
+        boolean watering(int quota) {
+            makeSpace();
+            if (this.leftQuota >= quota) {
+                this.leftQuota -= quota;
+                return true;
+            }
+            return false;
+        }
+    }
+    private Map<String, Funnel> funnels = new HashMap<>();
+    public boolean isActionAllowed(String userId, String actionKey, int capacity, float leakingRate) {
+        String key = String.format("%s:%s", userId, actionKey);
+        Funnel funnel = funnels.get(key);
+        if (funnel == null) {
+            funnel = new Funnel(capacity, leakingRate);
+            funnels.put(key, funnel);
+        }
+        return funnel.watering(1); // 需要 1 个 quota
+    }
+}
